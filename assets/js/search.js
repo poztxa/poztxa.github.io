3f371063daf8f82b08c68038dd05855170b9aa4e
jonnyleen <jonnyjakka@gmail.com>
2025-01-16 22:01:01 +0700

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,74 +1,138 @@
-function displaySearchResults(results, query) {
-    var searchResultsEl = document.getElementById("search-results"),
-        searchProcessEl = document.getElementById("search-process");
+---
+layout: null
+excluded_in_search: true
+---
+(function () {
+	function getQueryVariable(variable) {
+		var query = window.location.search.substring(1),
+			vars = query.split("&");
 
-    if (results.length) {
-        var resultsHTML = "";
-        var searchVersions = [{% for v in site.version_params.search_versions %}"{{ v }}",{% endfor %}"all"];
-        results.forEach(function (result) {
-            var item = window.data[result.ref];
+		for (var i = 0; i < vars.length; i++) {
+			var pair = vars[i].split("=");
 
-            // Versioning is disabled
-            if (("{{ site.version_params.versioning }}" == "false") && (item.version != "all")) {
-                return;
-            }
+			if (pair[0] === variable) {
+				return decodeURIComponent(pair[1].replace(/\+/g, '%20')).trim();
+			}
+		}
+	}
 
-            // Skip result if showing versions disabled
-            if (("{{ site.version_params.allow_search }}" == "false") && (item.version != "all")) {
-                return;
-            }
+	function getPreview(query, content, previewLength) {
+		previewLength = previewLength || (content.length * 2);
 
-            // Skip result if version not in all or versions allowed for search
-            if (("{{ site.version_params.versioning }}" == "true") && ("{{ site.version_params.allow_search }}" == "true") && (!searchVersions.includes(item.version))) {
-                return;
-            }
+		var parts = query.split(" "),
+			match = content.toLowerCase().indexOf(query.toLowerCase()),
+			matchLength = query.length,
+			preview;
 
-            if (item.title) {
-                var contentPreview = getPreview(query, item.content, 170),
-                    titlePreview = getPreview(query, item.title),
-                    versionBadge;
+		// Find a relevant location in content
+		for (var i = 0; i < parts.length; i++) {
+			if (match >= 0) {
+				break;
+			}
 
-                // If we only allow one version (all) skip adding a badge
-                if (searchVersions.length == 1 || "{{ site.version_params.versioning }}" == "false") {
-                    versionBadge = "";
-                } else if (item.version != "all") {
-                    versionBadge = "<span class='badge badge-secondary'>" + item.version + "</span>";
-                } else {
-                    versionBadge = "<span class='badge badge-{{ site.tag_color }}'>Current</span>";
-                }
+			match = content.toLowerCase().indexOf(parts[i].toLowerCase());
+			matchLength = parts[i].length;
+		}
 
-                // Ganti bagian ini dengan struktur artikel yang lebih kaya
-                resultsHTML += `
-                    <article class="blog-post hentry index-post">
-                        <a class="entry-image-wrap is-image" href="{{ site.baseurl }}${item.url.trim()}" title="${item.title}">
-                            ${item.image ? `<span class="entry-thumb lazy-ify" data-image="${item.image}" style="background-image:url(${item.image})"></span>` : ''}
-                        </a>
-                        <div class="entry-header">
-                            <span class="entry-category">${item.categories[0]}</span>
-                            <h2 class="entry-title">
-                                <a class="entry-title-link" href="{{ site.baseurl }}${item.url.trim()}" rel="bookmark" title="${item.title}">${titlePreview}</a>
-                            </h2>
-                            <p class="entry-excerpt excerpt">${contentPreview}</p>
-                            <div class="entry-meta">
-                                <span class="entry-author">
-                                    <span class="by">by</span>
-                                    <span class="author-name">${item.author ? site.authors[item.author].display_name : site.author}</span>
-                                </span>
-                                <span class="entry-time">
-                                    <span class="on">-</span>
-                                    <time class="published" datetime="${item.date}">${item.date}</time>
-                                </span>
-                            </div>
-                        </div>
-                    </article>
-                `;
-            }
-        });
+		// Create preview
+		if (match >= 0) {
+			var start = match - (previewLength / 2),
+				end = start > 0 ? match + matchLength + (previewLength / 2) : previewLength;
 
-        searchResultsEl.innerHTML = resultsHTML;
-        searchProcessEl.innerText = "";
-    } else {
-        searchResultsEl.style.display = "none";
-        searchProcessEl.innerText = "No results found.";
-    }
-}
+			preview = content.substring(start, end).trim();
+
+			if (start > 0) {
+				preview = "..." + preview;
+			}
+
+			if (end < content.length) {
+				preview = preview + "...";
+			}
+
+			// Highlight query parts
+			preview = preview.replace(new RegExp("(" + parts.join("|") + ")", "gi"), "<strong>$1</strong>");
+		} else {
+			// Use start of content if no match found
+			preview = content.substring(0, previewLength).trim() + (content.length > previewLength ? "..." : "");
+		}
+
+		return preview;
+	}
+
+	function displaySearchResults(results, query) {
+		var searchResultsEl = document.getElementById("search-results"),
+			searchProcessEl = document.getElementById("search-process");
+
+		if (results.length) {
+			var resultsHTML = "";
+			var searchVersions = [{% for v in site.version_params.search_versions %}"{{ v }}",{% endfor %}"all"]
+			results.forEach(function (result) {
+			
+  				var item = window.data[result.ref]
+
+				// Versioning is disabled
+				if (("{{ site.version_params.versioning }}" == "false") && (item.version != "all")) {
+				    return
+				}
+
+				// Skip result if showing versions disabled
+				if (("{{ site.version_params.allow_search }}" == "false") && (item.version != "all")) {
+				    return
+				}
+
+                               // Skip result if version not in all or versions allowed for search
+				if (("{{ site.version_params.versioning }}" == "true") && ("{{ site.version_params.allow_search }}" == "true") && (!searchVersions.includes(item.version))) {
+				    return
+				}
+
+                                if (item.title) {
+					contentPreview = getPreview(query, item.content, 170),
+					titlePreview = getPreview(query, item.title);
+
+					// If we only allow one version (all) skip adding a badge
+					if (searchVersions.length == 1 ||  "{{ site.version_params.versioning }}" == "false"){
+						versionBadge = ""
+
+					// Any older version shows up in gray
+					} else if (item.version != "all") {
+						versionBadge = "<span class='badge badge-secondary'>" + item.version + "</span>"
+
+					// Current is blue (primary)
+					} else {
+						versionBadge = "<span class='badge badge-{{ site.tag_color }}'>Current</span>"
+                                       }
+					resultsHTML += "<li><h4><a href='{{ site.baseurl }}" + item.url.trim() + "'>" + titlePreview + "</a></h4><p>" + versionBadge +"<small>" + contentPreview + "</small></p></li>";
+				}
+			});
+
+			searchResultsEl.innerHTML = resultsHTML;
+			searchProcessEl.innerText = "Showing";
+		} else {
+			searchResultsEl.style.display = "none";
+			searchProcessEl.innerText = "No";
+		}
+	}
+
+	window.index = lunr(function () {
+		this.field("id");
+		this.field("title", {boost: 10});
+		this.field("categories");
+		this.field("url");
+		this.field("content");
+	});
+
+	var query = decodeURIComponent((getQueryVariable("q") || "").replace(/\+/g, "%20")),
+		searchQueryContainerEl = document.getElementById("search-query-container"),
+		searchQueryEl = document.getElementById("search-query");
+
+	searchQueryEl.innerText = query;
+        if (query != ""){
+   		searchQueryContainerEl.style.display = "inline";
+        }
+
+	for (var key in window.data) {
+		window.index.add(window.data[key]);
+	}
+
+	displaySearchResults(window.index.search(query), query); // Hand the results off to be displayed
+})();
